9884e0143e1fa63498edef417dc39c769f5d4f9b
fillerInk <thatonephysicsnerd@gmail.com>
2020-05-15 20:58:10 +0530

    Added requirements and Procfile

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ from dash.dependencies import Input,Output,State
 import dash_core_components as dcc
 import dash_html_components as html
 import dash_bootstrap_components as dbc
-import dash_dangerously_set_inner_html as dds
 import plotly.graph_objs as go
 
 import os
